de76b3623a7c1cec12994f6d525461b9b037caed
noobash <54393997+noobash@users.noreply.github.com>
2023-08-12 12:03:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path : './config.env'});
+
+const app = require('./app');
+const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB,{
+    useNewUrlParser : true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+}).then(con => {
+    console.log("Successfully Connected!!!!");
+});
+
+const server = app.listen(port,()=>{
+console.log(`App is running on port ${port}...`);
+});
+
